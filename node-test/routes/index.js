c52c7da395502c61f02cf6670271a30e62cedc58
Usman Ghani <mghani828@gmail.com>
2020-09-03 04:39:46 +0500

    inital commit

--- node-test/routes/index.js
+++ node-test/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express is Changed' });
+  res.render('index', { title: 'Express  Changed' });
 });
 
 module.exports = router;
